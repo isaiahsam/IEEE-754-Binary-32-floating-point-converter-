7544e5025b012be459a0dc651e96cd5fd391fd3f
isaiahsam <isaiahpascual12@gmail.com>
2024-03-20 11:18:57 +0800

    design

--- mp/src/App.js
+++ mp/src/App.js
@@ -4,17 +4,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="content">
+          <h1>IEEE-754 Binary 32-Floating-point Converter</h1>
+          <p>
+            An IEEE-754 Binary 32-bit Floating-Point Converter translates decimal numbers into a 32-bit binary format, using specific bits for the sign, exponent, and significand, facilitating efficient real number handling in computing.
+          </p>
+          <div className="instructions">
+            <h2>How to use:</h2>
+            <ol>
+              <li>Enter the mantissa, choosing either decimal or binary format.</li>
+              <li>Select the numerical base: base 2 or base 10.</li>
+              <li>Provide the exponent value.</li>
+              <li>Press the "Convert" button to calculate and display the floating-point version of the provided data.</li>
+            </ol>
+          </div>
+        </div>
       </header>
     </div>
   );
